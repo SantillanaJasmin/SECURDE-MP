8856e595ed28043e7daeb898fddf6abad43b613b
SantillanaJasmin <jasminsantillana_cse@yahoo.com>
2016-08-15 00:26:18 +0800

    Modified sign up

--- SourceCode/src/java/controller/UserController.java
+++ SourceCode/src/java/controller/UserController.java
@@ -13,35 +13,41 @@ import model.User;
  * @author Joy
  */
 public class UserController {
+
     private boolean valid = false;
-    
+    private boolean isAdded = false;
+
     public UserController() {
-        
+
     }
-    
+
     public boolean signIn(String username, String password) {
         UserDB userDB = new UserDB();
         User user = userDB.signIn(username);
         PasswordHashing ph = new PasswordHashing();
-        if(user.getUsername().equals(username)) {
-            if(ph.checkPassword(password, user.getPassword())) {
+        if (user.getUsername().equals(username)) {
+            if (ph.checkPassword(password, user.getPassword())) {
                 valid = true;
             } else { /* password is not correct */
-                user.setAttempts(user.getAttempts()+1);
+
+                user.setAttempts(user.getAttempts() + 1);
                 userDB.setLoginAttempt(user);
             }
-        } 
-        
+        }
+
         return valid;
     }
     /* compile details of the user in the servlet */
-    public boolean signUp(String firstName, String middleInitial, String lastName, 
-            String username, String password, String email, int accountType) {
+
+    public boolean signUp(String firstName, String middleInitial, String lastName,
+        String username, String password, String email, int accountType) {
         UserDB userDB = new UserDB();
         PasswordHashing ph = new PasswordHashing();
         password = ph.PasswordHashing(password);
-        User user = new User(firstName, middleInitial, lastName, 
+        User user = new User(firstName, middleInitial, lastName,
                 username, password, email, accountType);
-        return userDB.addUser(user);
+        
+        isAdded = userDB.addUser(user);
+        return isAdded;
     }
 }
